8325a99af859314a6a1c21ec06f5340c98268f2d
aleaOneHundredDays <59572864+aleaOneHundredDays@users.noreply.github.com>
2020-01-24 14:15:06 +0000

    Add files via upload

---
+++ ClassesAndConstructors/ClassesAndConstructors/PrivateClassExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndConstructors
+{
+    class PrivateClassExample
+    {
+    }
+
+    public class Counter
+    {
+        private Counter() { }
+        public static int currentCount;
+        public static int IncrementCount()
+        {
+            return ++currentCount;
+        }
+    }
+}
